ffc192f86b976e32ca0bdbe1d0ec5d96d02ee756
carmine <carminepepiciello@gmail.com>
2020-11-30 13:29:48 +0100

    primo push e pausa pranzo

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+
+        menus:['home','pages','program','tickets','speakers','papers','blog','shortcodes'],
+        questions:[
+            {
+                title:'Who we are',
+                text:'Donec id elit non mi porta gravida at eget me tus. Sed posuere consectetur estat lobo rtis. Cum sociis natoque.',
+                link:'Learn More',
+                icon:'fas fa-trophy'
+            },
+            {
+                title:'Who we are',
+                text:'Donec id elit non mi porta gravida at eget me tus. Sed posuere consectetur estat lobo rtis. Cum sociis natoque.',
+                link:'Learn More',
+                icon:'fas fa-flash'
+            },
+            {
+                title:'Why us?',
+                text:'Donec id elit non mi porta gravida at eget me tus. Sed posuere consectetur estat lobo rtis. Cum sociis natoque.',
+                link:'Learn More',
+                icon:'fas fa-question'
+            }
+        ]
+    },
+    methods: {
+
+    },
+    mounted() {
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+});
