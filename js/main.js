e460ce3b4d20fb293cf04541b2a182435808b6e4
carmine <carminepepiciello@gmail.com>
2020-11-30 17:10:08 +0100

    speaker section

--- js/main.js
+++ js/main.js
@@ -14,13 +14,35 @@ var app = new Vue ({
                 title:'Who we are',
                 text:'Donec id elit non mi porta gravida at eget me tus. Sed posuere consectetur estat lobo rtis. Cum sociis natoque.',
                 link:'Learn More',
-                icon:'fas fa-flash'
+                icon:'fas fa-bolt'
             },
             {
                 title:'Why us?',
                 text:'Donec id elit non mi porta gravida at eget me tus. Sed posuere consectetur estat lobo rtis. Cum sociis natoque.',
                 link:'Learn More',
                 icon:'fas fa-question'
+            }
+        ],
+        speakers:[
+            {
+                img:'speaker-8',
+                title:'Laurence Francis',
+                text:'Google\'s UI Designer',
+            },
+            {
+                img:'speaker-7',
+                title:'John Super Doe',
+                text:'Yahoo\'s HR',
+            },
+            {
+                img:'speaker-6',
+                title:'Patrick Spencer',
+                text:'S&P Analyzer',
+            },
+            {
+                img:'speaker-5',
+                title:'Janet Jones',
+                text:'Newyork Post\'s GM',
             }
         ]
     },
